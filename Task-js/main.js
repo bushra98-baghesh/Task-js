e015d293ce5fbf695bc0063651c5aa2ecc749f01
bushra98-baghesh <85075834+bushra98-baghesh@users.noreply.github.com>
2022-08-26 21:21:37 +0300

    Update main.js

--- Task-js/main.js
+++ Task-js/main.js
@@ -19,8 +19,8 @@ class Student {
   constructor(studentFirstName, studentLastName, schoolName, sectionNumber) {
     this.studentFirstName = studentFirstName;
     this.studentLastName = studentLastName;
-    this.school = new School(schoolName);
-    this.section = new Section(sectionNumber);
+    this.school = schoolName.nameofschool();
+    this.section = sectionNumber.numberofsections();
     Student.count++;
   }
   describe() {
@@ -30,18 +30,19 @@ class Student {
         " \n student last name:" +
         this.studentLastName +
         "\n school name:" +
-        this.school.nameofschool() +
+        this.school +
         "\n section Number : " +
-        this.section.numberofsections()
+        this.section
     );
   }
 }
-
+let schools= [new School("art"), new School("programming"), new School("archeticture")];
+let sections= [new Section("1"), new Section("2"),new Section("3")];
 let students = [
-  new Student("ali", "mhd", "art", "3"),
-  new Student("sameh", "mhd", "programming", "2"),
-  new Student("samer", "mhd", "archeticture", "1"),
-  new Student("yara", "mhd", "art", "3"),
+  new Student("ali", "mhd", schools[0], sections[2]),
+  new Student("sameh", "mhd", schools[1], sections[1]),
+  new Student("samer", "mhd", schools[2], sections[0]),
+  new Student("yara", "mhd", schools[0], sections[2]),
 ];
 function getStudent(students) {
   for (let student of students) {
